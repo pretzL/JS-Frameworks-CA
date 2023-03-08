86a7edf0f1856706cfeb19d9eb3844a6b2240a26
pretzL <ericpretzinger@gmail.com>
2023-03-08 20:35:41 +0100

    Initial file setup and folder structure

---
+++ src/pages/index.jsx
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Product } from "./Product";
+export { default as Cart } from "./Cart";
+export { default as Checkout } from "./Checkout";
+export { default as Contact } from "./Contact";
+export { default as NotFound } from "./404";
