d9173634ed0f3749af3868342df99b2663b90e57
Eric Pretzinger <eric.pretzinger@kabal.com>
2023-03-09 08:49:16 +0100

    Adjust body size, add divider to Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "../../components";
+import styles from "./Home.module.css";
 
 export const Home = () => {
     return (
         <main>
             <Hero />
+            <div className={styles.divider}>Some kind of sloagan here.</div>
         </main>
     );
 };
