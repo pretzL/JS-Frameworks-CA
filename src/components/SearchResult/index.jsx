7b4103cee8ceb0bce289b305d020b6183a1be8ca
pretzL <ericpretzinger@gmail.com>
2023-03-21 22:20:11 +0100

    Fixed responsiveness up until cart/checkout/contact

--- src/components/SearchResult/index.jsx
+++ src/components/SearchResult/index.jsx
@@ -7,7 +7,7 @@ export const SearchResult = ({ data, onClick }) => {
             <div className={styles.item} onClick={onClick}>
                 <img src={data.imageUrl} alt={data.title} className={styles.itemImg} />
                 <h4>{data.title}</h4>
-                <p className={styles.itemPrice}>{data.price}</p>
+                <p className={styles.itemPrice}>${data.price}</p>
             </div>
         </Link>
     );
