cce789ea57a9b195298ce30442d04c975b438c39
Eric Pretzinger <eric.pretzinger@kabal.com>
2023-03-24 13:46:34 +0100

    Adds partial error/loading state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,18 +5,23 @@ import styles from "./Home.module.css";
 export const Home = () => {
     const { data, isLoading, isError } = useFetch("https://api.noroff.dev/api/v1/online-shop");
 
+    let content;
+    if (isError) {
+        content = <div className="error">There was an error loading the data.</div>;
+    } else if (isLoading) {
+        content = <div>Loading...</div>;
+    } else {
+        content = data.map((item) => <Card data={item} key={item.id} />);
+    }
+
     return (
         <main>
             <Hero />
-            <div className={styles.divider}>Some kind of sloagan here.</div>
+            <div className={styles.divider}>Some kind of slogan here.</div>
             <div className={styles.heroSmall}></div>
             <section className={styles.contentSection}>
                 <h2>Highest Rated</h2>
-                <div className={styles.productContainer}>
-                    {data.map((item) => (
-                        <Card data={item} key={item.id} />
-                    ))}
-                </div>
+                <div className={styles.productContainer}>{content}</div>
             </section>
         </main>
     );
