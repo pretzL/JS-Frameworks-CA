86faceeb7a363c03d7f27fe2bce590d56f6ab75b
pretzL <ericpretzinger@gmail.com>
2023-03-30 21:33:52 +0200

    Fix import

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,7 +3,7 @@ import tshirts from "../../assets/images/tshirts.png";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { ScreenLabel } from "../../components/ScreenLabel";
+import { ScreenLabel } from "../../components";
 import { useState } from "react";
 
 const schema = yup
